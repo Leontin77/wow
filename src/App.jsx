1b34ac84479c7c115bb9b2cd42c2ef7b54577ec8
DmytroSokolovskyi <dim4uk@gmail.com>
2024-04-21 21:23:42 +0300

    sdsd

--- src/App.jsx
+++ src/App.jsx
@@ -18,14 +18,14 @@ function App() {
     tg.expand(); //расширяем на все окно
 
 tg.MainButton.text = "Changed Text"; //изменяем текст кнопки
-tg.MainButton.setText("Changed Text1"); //изменяем текст кнопки иначе
 tg.MainButton.textColor = "#F55353"; //изменяем цвет текста кнопки
 tg.MainButton.color = "#143F6B"; //изменяем цвет бэкграунда кнопки
 tg.MainButton.setParams({"color": "#143F6B"}); //так изменяются все параметры
 
 
-    Telegram.WebApp.onEvent('mainButtonClicked', function(){
+tg.onEvent('mainButtonClicked', function(){
 	tg.sendData("some string that we need to send");
+	tg.sendMessage("!@!@!@!@!@!@ send");
 	//при клике на основную кнопку отправляем данные в строковом виде
 });
 
