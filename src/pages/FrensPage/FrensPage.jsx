d9664a07d4253ea27665d57822621a2e34ed78b9
Leontin Chechur <chechur.leontin@gmail.com>
2024-05-19 13:22:39 +0300

    added icon

--- src/pages/FrensPage/FrensPage.jsx
+++ src/pages/FrensPage/FrensPage.jsx
@@ -2,16 +2,19 @@ import "./FrensPage.scss";
 import FransItem from "../../components/FrensItem/FrensItem";
 import FrensClaim from "../../components/FrensClaim/FrensClaim";
 import BaseButton from "../../components/BaseButton/BaseButton";
+import { FaUsersViewfinder } from "react-icons/fa6";
 
-const FransPage = () => {
+
+const FrensPage = () => {
   return (
-    <>
+    <div className="frensPage">
+      <FaUsersViewfinder size='5em'/>
       <h2>Invite Frens</h2>
       <FrensClaim/>
       <FransItem />
       <BaseButton title="Invite a fren" className="frensPage-button"/>
-    </>
+    </div>
   );
 };
 
-export default FransPage;
+export default FrensPage;
